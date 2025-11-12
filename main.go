c5b22d9de65444da8ab11f99696e631f4bd56934
kidus-m <kidus0237@gmail.com>
2025-11-12 20:55:19 +0300

    go main

--- main.go
+++ main.go
@@ -1,16 +1,56 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"time"
 
-	"task_manager/router"
+	"github.com/joho/godotenv"
+	"go_mango/controllers"
+	"go_mango/data"
+	"go_mango/router"
 )
 
 func main() {
-	r := router.SetupRouter()
+	// Load .env if present
+	_ = godotenv.Load()
 
-	// start server on port 8080
-	if err := r.Run(":8080"); err != nil {
+	mongoURI := os.Getenv("MONGODB_URI")
+	dbName := os.Getenv("MONGODB_DATABASE")
+	collectionName := os.Getenv("MONGODB_COLLECTION")
+	if mongoURI == "" || dbName == "" || collectionName == "" {
+		log.Fatal("MONGODB_URI, MONGODB_DATABASE and MONGODB_COLLECTION must be set (see .env.example)")
+	}
+
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := data.NewMongoClient(ctx, mongoURI)
+	if err != nil {
+		log.Fatalf("failed to create mongo client: %v", err)
+	}
+
+	// Close client on exit
+	defer func() {
+		_ = client.Disconnect(context.Background())
+	}()
+
+	collection := client.Database(dbName).Collection(collectionName)
+	taskService := data.NewTaskService(collection)
+	taskController := controllers.NewTaskController(taskService)
+
+	r := router.SetupRouter(taskController)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+	if err := r.Run(addr); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
 }
